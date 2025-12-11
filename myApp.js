c8a1bf2fc8f0fbf5b3847ca54890c5b7981c0f73
Evallejos96 <96029483+Evallejos96@users.noreply.github.com>
2025-12-11 16:55:46 -0300

    Update myApp.js -- original con Helmet

--- myApp.js
+++ myApp.js
@@ -4,11 +4,59 @@ const app = express();
 const helmet = require('helmet');
 app.use(helmet());
 
-app.use(express.static('public'));
-app.disable('strict-transport-security');
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
